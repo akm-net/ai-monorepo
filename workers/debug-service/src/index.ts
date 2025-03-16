491ebe254bc3d69536b56a9d8e3aa27a6df0181a
muhammad nur akmal <muhammad_nur_akmal_zubir@cpf.gov.sg>
2025-03-16 16:20:05 +0800

    update

--- workers/debug-service/src/index.ts
+++ workers/debug-service/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono'
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
 app.get('/', (c) => {
+  const { TESTDEVVAR } = env<{ TESTDEVVAR: string }>(c)
+  console.log(TESTDEVVAR)
   return c.text('Hello Hono!')
 })
 
