938c8ffc5fb3f6312e0c673cf7c31e2e0ad26961
muhammad nur akmal <muhammad_nur_akmal_zubir@cpf.gov.sg>
2025-03-16 16:33:12 +0800

    test

--- workers/debug-service/src/index.ts
+++ workers/debug-service/src/index.ts
@@ -1,11 +1,19 @@
-import { Hono } from 'hono'
-import { env } from 'hono/adapter'
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  const { TESTDEVVAR } = env(c)
-  return c.text(TESTDEVVAR)
-})
-
-export default app
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		console.log(env.TESTDEVVAR)
+		return new Response('Hello World!');
+	},
+} satisfies ExportedHandler<Env>;
