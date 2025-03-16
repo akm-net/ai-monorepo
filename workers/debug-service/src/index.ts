82b608f67ec382fec7a8b5e422f8b4fe202c1819
muhammad nur akmal <muhammad_nur_akmal_zubir@cpf.gov.sg>
2025-03-16 16:30:12 +0800

    test

--- workers/debug-service/src/index.ts
+++ workers/debug-service/src/index.ts
@@ -4,9 +4,8 @@ import { env } from 'hono/adapter'
 const app = new Hono()
 
 app.get('/', (c) => {
-  const { TESTDEVVAR } = env<{ TESTDEVVAR: string }>(c)
-  console.log(c.text(TESTDEVVAR))
-  return c.text('Hello Hono!')
+  const { TESTDEVVAR } = env(c)
+  return c.text(TESTDEVVAR)
 })
 
 export default app
